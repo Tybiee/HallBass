6677796c71c11a4f609d6fbc105a13cb5a55825a
Tybiee <tybiemeyer@gmail.com>
2022-11-30 23:23:02 -0500

    Preliminary Structure

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
+# Install PyQt using the following command: "pip install PyQt5" after installing python on PATH
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = QWidget()
